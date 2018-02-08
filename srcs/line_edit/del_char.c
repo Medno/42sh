df02aa9f7c54f28f0b0eaa805426d4bae869c688
Philippe CHADENIER <pchadeni@e1r10p15.42.fr>
2018-02-08 17:16:34 +0100

    move ok, treatment of deleting over several raws

--- srcs/line_edit/del_char.c
+++ srcs/line_edit/del_char.c
@@ -6,7 +6,7 @@
 /*   By: pchadeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 14:51:33 by pchadeni          #+#    #+#             */
-/*   Updated: 2018/02/08 14:17:36 by kyazdani         ###   ########.fr       */
+/*   Updated: 2018/02/08 17:14:34 by pchadeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,33 @@ static void	moove_curs(t_line *current, int prompt)
 	}
 }
 
+void		print_del(t_line *cur, int len, int prompt)
+{
+	t_line	*tmp;
+	char	buf[len];
+	int		i;
+
+	i = 0;
+	tmp = cur;
+	ft_bzero(buf, len);
+	while (tmp->next)
+	{
+		buf[i] = tmp->c;
+		tmp = tmp->next;
+		i++;
+	}
+	write(STDIN_FILENO, &buf, i);
+/*	while (i)
+	{
+		tmp = move_left(tmp, len);
+		i--;
+	}*/
+}
+
 t_line		*line_delone(t_line *cur, int len)
 {
 	t_line	*del;
+	t_line	*tmp;
 
 	del = cur->prev;
 	if (del)
@@ -49,7 +73,10 @@ t_line		*line_delone(t_line *cur, int len)
 			del->prev->next = cur;
 		del_one_elem(del);
 		increment_all(cur, -1);
+		tputs(tgetstr("cd", NULL), 0, &ft_inputchar);
 		moove_curs(cur, len);
+		if (cur->next)
+			print_del(cur, dblist_len(cur), len);
 	}
 	return (cur);
 }
