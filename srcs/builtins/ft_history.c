f427ddf7bb73ea294c71de504e0e2efee4a26c0a
Kiyan YAZDANI <kyazdani@e2r3p14.42.fr>
2018-03-14 11:41:44 +0100

    -a -n -p are not history options anymore (useless)

--- srcs/builtins/ft_history.c
+++ srcs/builtins/ft_history.c
@@ -6,7 +6,7 @@
 /*   By: kyazdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 11:41:13 by kyazdani          #+#    #+#             */
-/*   Updated: 2018/03/07 15:00:42 by kyazdani         ###   ########.fr       */
+/*   Updated: 2018/03/14 11:40:25 by kyazdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,56 +17,33 @@ extern char	*g_optarg;
 
 static int	set_bits(int c, int fl)
 {
-	if (c == 'a')
+	if (c == 'w')
 		fl |= 1;
-	else if (c == 'w')
-		fl |= 2;
 	else if (c == 'r')
-		fl |= 4;
-	else if (c == 'n')
-		fl |= 8;
-	else if (c == 'p')
-		fl |= 16;
+		fl |= 2;
 	else if (c == 's')
-		fl |= 32;
+		fl |= 4;
 	else if (c == 'c')
-		fl |= 64;
+		fl |= 8;
 	else if (c == 'd')
-		fl |= 128;
+		fl |= 16;
 	return (fl);
-}
-
-static int	check_bits(int fl)
-{
-	if ((1 & fl) && ((2 & fl) || (4 & fl) || (8 & fl)))
-		return (1);
-	else if ((2 & fl) && ((4 & fl) || (8 & fl)))
-		return (1);
-	else if ((4 & fl) && (8 & fl))
-		return (1);
-	return (0);
 }
 
 static int		step_2(t_hist **histo, char **str, int flags)
 {
 	if (!flags)
 		return (print_history(histo, str));
-	if (64 & flags)
+	if (8 & flags)
 		return (free_history(histo));
-	if (128 & flags)
+	if (16 & flags)
 		return (free_offset_hist(histo, g_optarg));
 	if (1 & flags)
-		return (0);//a
-	else if (2 & flags)
 		return (full_hist_file(histo, *str));
-	else if (4 & flags)
+	if (2 & flags)
 		return (append_to_list(histo, *str));
-	else if (8 & flags)
-		return (0);//n
-	if (32 & flags)
+	if (4 & flags)
 		return (replace_w_arg(histo, str));
-	if (16 & flags)
-		return (0);//p
 	return (0);
 }
 
@@ -78,20 +55,20 @@ int		ft_history(t_init *init, char ***entry)
 	flags = 0;
 	c = 0;
 	reset_ft_opt();
-	while ((c = ft_getopt(ft_tablen(*entry), *entry, "carnpswd:")) != -1)
+	while ((c = ft_getopt(ft_tablen(*entry), *entry, "crswd:")) != -1)
 	{
-		if (ft_strchr("carnpswd", (char)c))
+		if (ft_strchr("crswd", (char)c))
 			flags = set_bits(c, flags);
 		else if (c == ':' || c == '?')
 		{
 			ft_putstr_fd("history: usage: history [-c] [-d offset] [n] or \
-history -awrn [filename] or history -ps arg [arg...]\n", STDERR_FILENO);
+history -wr [filename] or history -s arg [arg...]\n", STDERR_FILENO);
 			return (1);
 		}
 	}
-	if (check_bits(flags))
+	if ((1 & flags) && (2 & flags))
 	{
-		ft_putendl_fd("42sh: history: cannot use more than one of -anrw", 2);
+		ft_putendl_fd("42sh: history: cannot use more than one of -rw", 2);
 		return (1);
 	}
 	else
