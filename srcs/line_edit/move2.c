b5d3a112055844265acfe9d8b5255bd969e2d218
Kiyan YAZDANI <kyazdani@e2r3p14.42.fr>
2018-03-15 11:09:32 +0100

    opti first last

--- srcs/line_edit/move2.c
+++ srcs/line_edit/move2.c
@@ -6,7 +6,7 @@
 /*   By: kyazdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/10 11:13:34 by kyazdani          #+#    #+#             */
-/*   Updated: 2018/03/14 10:13:05 by kyazdani         ###   ########.fr       */
+/*   Updated: 2018/03/15 11:08:33 by kyazdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,20 @@
 t_line		*move_first(t_line *cur, t_curs *curseur)
 {
 	check_ynx(curseur, cur->index);
-	check_max(curseur, last_index(cur));
+	if (curseur->y > 0 && curseur->x)
+		ansi("UP", curseur->y, STDIN_FILENO);
 	while (cur->prev)
-		cur = move_left(cur, curseur);
+		cur = cur->prev;
+	if (!curseur->x)
+	{
+		ansi("LE", curseur->screen.ws_col - cur->index, STDIN_FILENO);
+		if (curseur->y > 1)
+			ansi("UP", curseur->y - 1, STDIN_FILENO);
+	}
+	else if (curseur->x > cur->index)
+		ansi("LE", curseur->x - cur->index, STDIN_FILENO);
+	else if (curseur->x < cur->index)
+		ansi("RI", cur->index - curseur->x, STDIN_FILENO);
 	return (cur);
 }
 
@@ -25,8 +36,14 @@ t_line		*move_last(t_line *cur, t_curs *curseur)
 {
 	check_ynx(curseur, cur->index);
 	check_max(curseur, last_index(cur));
+	if (curseur->x > curseur->xmax + 1)
+		ansi("LE", curseur->x - curseur->xmax - 1, STDIN_FILENO);
+	else if (curseur->x < curseur->xmax + 1)
+		ansi("RI", curseur->xmax - curseur->x + 1, STDIN_FILENO);
+	if (curseur->ymax > curseur->y)
+		ansi("DO", curseur->ymax - curseur->y, STDIN_FILENO);
 	while (cur->next)
-		cur = move_right(cur, curseur);
+		cur = cur->next;
 	return (cur);
 }
 
