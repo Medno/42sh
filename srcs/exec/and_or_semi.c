16610096d0d33fbd95dd823de02bf3b092c07670
Hugo LELY <hlely@student.42.fr>
2018-03-15 14:09:24 +0100

    fix return value of commands + clean funct export

--- srcs/exec/and_or_semi.c
+++ srcs/exec/and_or_semi.c
@@ -6,7 +6,7 @@
 /*   By: hlely <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 10:25:58 by hlely             #+#    #+#             */
-/*   Updated: 2018/03/15 10:00:06 by hlely            ###   ########.fr       */
+/*   Updated: 2018/03/15 14:03:37 by hlely            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 void		launch_and(t_init *init, t_ast *ast, int std_fd[])
 {
 	int		ret;
+	int		status;
 
-	if (!(ret = launch_exec(init, ast->left, std_fd)))
+	ret = launch_exec(init, ast->left, std_fd);
+	status = wait_pipe(&init->pid_list);
+	reset_fd(std_fd, ast->left, RESETALL);
+	if (status != -1)
+		ret = status;
+	if (!ret)
 	{
-		wait_pipe(&init->pid_list);
-		reset_fd(std_fd, ast->left, RESETALL);
 		launch_exec(init, ast->right, std_fd);
 		wait_pipe(&init->pid_list);
 		reset_fd(std_fd, ast->right, RESETALL);
@@ -29,11 +33,15 @@ void		launch_and(t_init *init, t_ast *ast, int std_fd[])
 void		launch_or(t_init *init, t_ast *ast, int std_fd[])
 {
 	int		ret;
+	int		status;
 
-	if ((ret = launch_exec(init, ast->left, std_fd)))
+	ret = launch_exec(init, ast->left, std_fd);
+	status = wait_pipe(&init->pid_list);
+	reset_fd(std_fd, ast->left, RESETALL);
+	if (status != -1)
+		ret = status;
+	if (ret)
 	{
-		wait_pipe(&init->pid_list);
-		reset_fd(std_fd, ast->left, RESETALL);
 		launch_exec(init, ast->right, std_fd);
 		wait_pipe(&init->pid_list);
 		reset_fd(std_fd, ast->right, RESETALL);
