ad2fa1e6cb07099537765e8ccf3c289f69329850
Kiyan YAZDANI <kyazdani@e1r10p12.42.fr>
2018-03-19 10:54:27 +0100

    path checking for binaries in /Users/kyazdani/.brew/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/munki

--- srcs/exec/ft_check_path.c
+++ srcs/exec/ft_check_path.c
@@ -6,34 +6,13 @@
 /*   By: kyazdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 15:07:47 by kyazdani          #+#    #+#             */
-/*   Updated: 2018/03/18 10:50:22 by kyazdani         ###   ########.fr       */
+/*   Updated: 2018/03/19 10:51:33 by kyazdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-static int	is_accessible(char *str)
-{
-	struct stat	buf;
-
-	if (!access(str, F_OK))
-	{
-		if (!lstat(str, &buf))
-		{
-			if (S_IFREG & buf.st_mode)
-			{
-				if (S_IXUSR & buf.st_mode)
-					return (0);
-				return (-2);
-			}
-			return (-3);
-		}
-		return (-2);
-	}
-	return (-1);
-}
-
-static int	check_bin(char *str, t_env **env, char **s_fin)
+static int	check_bin(char *str, t_env **env, char **s_fin, int print)
 {
 	int		ret;
 	int		i;
@@ -48,16 +27,15 @@ static int	check_bin(char *str, t_env **env, char **s_fin)
 	{
 		tmp = ft_strjoin(path[i], "/");
 		tmp = ft_strjoindel(tmp, str);
-		if (!(ret = is_accessible(tmp)))
-		{
-			*s_fin = ft_strdup(tmp);
+		if (!(ret = check_slash(tmp, s_fin, NOPRINT)) || ret == -2)
 			break ;
-		}
 		ft_strdel(&tmp);
 	}
 	ft_strdel(&tmp);
 	ft_freetab(path);
-	if (ret)
+	if (ret == -2)
+		return (check_error(ret, str, print));
+	else if (ret)
 		ret = 1;
 	return (ret);
 }
@@ -74,7 +52,7 @@ int			check_path(char **arg, t_env **env, char **s_fin, int print)
 	if (ft_strchr(*arg, '/'))
 		return (check_slash(*arg, s_fin, print));
 	else if (ft_getenv(env, "PATH"))
-		return (check_bin(*arg, env, s_fin));
+		return (check_bin(*arg, env, s_fin, print));
 	else
 		return (check_error(-1, *arg, print));
 }
