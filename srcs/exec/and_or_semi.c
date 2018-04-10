3779667b4208cf13e13850766838af4482945bf4
Hugo LELY <hlely@student.42.fr>
2018-04-10 17:10:20 +0200

    fix compilation

--- srcs/exec/and_or_semi.c
+++ srcs/exec/and_or_semi.c
@@ -6,7 +6,7 @@
 /*   By: hlely <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 10:25:58 by hlely             #+#    #+#             */
-/*   Updated: 2018/03/20 17:45:53 by hlely            ###   ########.fr       */
+/*   Updated: 2018/04/10 17:10:01 by hlely            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ void		launch_and(t_init *init, t_ast *ast, int std_fd[], int error)
 
 	ret = launch_exec(init, ast->left, std_fd, error);
 	reset_fd(std_fd);
-	status = wait_pipe(&init->pid_list);
+	status = wait_pipe(&init->pid_list, ret);
 	if (status != -1)
 		ret = status;
 	error = ret;
 	if (!ret || (ast->right && ast->right->value == OR_IF))
 	{
-		launch_exec(init, ast->right, std_fd, error);
+		ret = launch_exec(init, ast->right, std_fd, error);
 		reset_fd(std_fd);
-		wait_pipe(&init->pid_list);
+		wait_pipe(&init->pid_list, ret);
 	}
 }
 
@@ -40,20 +40,20 @@ void		launch_or(t_init *init, t_ast *ast, int std_fd[], int error)
 	{
 		ret = launch_exec(init, ast->left, std_fd, error);
 		reset_fd(std_fd);
-		status = wait_pipe(&init->pid_list);
+		status = wait_pipe(&init->pid_list, ret);
 		if (status != -1)
 			ret = status;
 		if (ret)
 		{
-			launch_exec(init, ast->right, std_fd, error);
+			ret = launch_exec(init, ast->right, std_fd, error);
 			reset_fd(std_fd);
-			wait_pipe(&init->pid_list);
+			wait_pipe(&init->pid_list, ret);
 		}
 	}
 	else
 	{
-		launch_exec(init, ast->right, std_fd, error);
+		ret = launch_exec(init, ast->right, std_fd, error);
 		reset_fd(std_fd);
-		wait_pipe(&init->pid_list);
+		wait_pipe(&init->pid_list, ret);
 	}
 }
