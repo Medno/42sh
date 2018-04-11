e33f52850a517f661a7de5dcd97ed8f9a14291df
Medno <phil.chadenier@gmail.com>
2018-04-11 11:59:21 +0200

    Edit dollar

--- srcs/expansion/dollar_get.c
+++ srcs/expansion/dollar_get.c
@@ -6,7 +6,7 @@
 /*   By: pchadeni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 17:16:09 by pchadeni          #+#    #+#             */
-/*   Updated: 2018/04/10 17:16:29 by pchadeni         ###   ########.fr       */
+/*   Updated: 2018/04/11 11:58:29 by pchadeni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 
 static char	incr_index_in_esc(char *str, int *i, char esc)
 {
-	if ((str[*i] && str[*i] == '\\'))
-		(*i) += 2;
-	else if (esc == '\\')
-		(*i)++;
 	while (str[*i] && str[*i] != esc && esc == '\'')
 		(*i)++;
-	if (esc == '\\' && str[*i] == '\"')
-		esc = str[*i];
-	if (esc != '\"')
+	if (esc == '\'')
 		esc = 0;
 	return (esc);
+}
+
+static void	check_escape(char *str, int *i, char *escape)
+{
+	if (*escape == '\\')
+		*escape = 0;
+	else if (*escape && str[*i] == *escape)
+	{
+		*escape = 0;
+		(*i)++;
+	}
+	if (!*escape && str[*i] && is_quote_bslash(str[*i]))
+	{
+		*escape = str[*i];
+		(*i)++;
+	}
 }
 
 int			get_unquoted_dollar(char *str, int i, int *rep)
@@ -33,19 +43,14 @@ int			get_unquoted_dollar(char *str, int i, int *rep)
 
 	while (str[i])
 	{
-		if (escape && str[i] == escape)
-			escape = 0;
-		else if (!escape && is_quote_bslash(str[i]))
-		{
-			escape = str[i];
-			i++;
-		}
-		if (escape == '\'' || escape == '\\' || (str[i] && str[i] == '\\'))
+		check_escape(str, &i, &escape);
+		if (escape == '\'')
 			escape = incr_index_in_esc(str, &i, escape);
 		if (str[i] && str[i + 1] && str[i] == '$' && str[i + 1] == '$')
 			i++;
 		*rep = (str[i] && str[i] == '$' && escape == '\"') ? 2 : *rep;
-		if (str[i] && str[i] == '$' && str[i + 1] && str[i + 1] != ' ')
+		if (str[i] && str[i] == '$' && str[i + 1] && str[i + 1] != ' ' &&
+				escape != '\\')
 			return (i);
 		if (str[i])
 			i++;
