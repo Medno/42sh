db9ebd0d96df22d29520b766cd3ab40eea39d293
Hugo LELY <hlely@student.42.fr>
2018-04-11 08:13:33 +0200

    fix exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: kyazdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/07 09:57:12 by kyazdani          #+#    #+#             */
-/*   Updated: 2018/04/10 17:23:23 by hlely            ###   ########.fr       */
+/*   Updated: 2018/04/11 08:11:35 by hlely            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	final_exit(t_init *init, unsigned char x, char **entry)
 	hist_to_file(init->historic);
 	ft_strdel(&init->str);
 	free_list(&init->new_env);
-	if (x != 255)
+	if (x == 0)
 		x = g_status;
 	exit(x);
 	return (0);
