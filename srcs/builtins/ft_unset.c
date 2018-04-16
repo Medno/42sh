b08d3855650e73248dc4fb24dbac1efaff8c9ef3
Hugo LELY <hlely@student.42.fr>
2018-04-16 11:44:49 +0200

    handling options with unset

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,18 +6,53 @@
 /*   By: hlely <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/11 13:57:14 by hlely             #+#    #+#             */
-/*   Updated: 2018/03/19 16:00:52 by hlely            ###   ########.fr       */
+/*   Updated: 2018/04/16 11:39:59 by hlely            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-int			ft_unset(t_env **loc, t_env **env, char **arg)
+static int		unset_usage(void)
+{
+	ft_printf_fd(STDERR_FILENO, "21sh: usage: unset [-f] [-v] [name...]\n");
+	return (1);
+}
+
+static int		set_bits(char c, int flag)
+{
+	if (c == 'f')
+		flag |= 1;
+	else if (c == 'v')
+		flag |= 2;
+	return (flag);
+}
+
+static int		not_real_unset(char **arg, int flags)
 {
 	int		i;
 	int		ret;
 
-	i = 0;
+	i = flags ? 2 : 1;
+	ret = 0;
+	while (arg[i])
+	{
+		if (!is_all_valid_identifier("unset", arg[i], PRINT))
+		{
+			i++;
+			ret = 1;
+			continue ;
+		}
+		i++;
+	}
+	return (ret);
+}
+
+static int		unset_for_real(t_env **loc, t_env **env, char **arg, int flags)
+{
+	int		i;
+	int		ret;
+
+	i = flags ? 2 : 1;
 	ret = 0;
 	while (arg[i])
 	{
@@ -34,4 +69,29 @@ int			ft_unset(t_env **loc, t_env **env, char **arg)
 		i++;
 	}
 	return (ret);
+}
+
+int				ft_unset(t_env **loc, t_env **env, char **arg)
+{
+	int		flags;
+	char	c;
+
+	flags = 0;
+	c = 0;
+	reset_ft_opt();
+	while ((c = ft_getopt(ft_tablen(arg), arg, "fv")) != -1)
+	{
+		if (ft_strchr("fv", c))
+			flags = set_bits(c, flags);
+		else if (c == '?')
+			return (unset_usage());
+	}
+	if ((flags & 1) && (flags & 2))
+	{
+		ft_printf_fd(STDERR_FILENO, "21sh: unset: cannot have flag f and v\n");
+		return (1);
+	}
+	if (flags & 2 || flags == 0)
+		return (unset_for_real(loc, env, arg, flags));
+	return (not_real_unset(arg, flags));
 }
