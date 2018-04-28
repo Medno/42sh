b69b0abbe9c76f831d36fbb59d9ebe89674209bf
Hugo FOUQUES <hfouques@e1r2p5.42.fr>
2018-04-28 13:44:07 +0200

    Fix heredoc with backslash

--- srcs/parser/repeat_heredoc.c
+++ srcs/parser/repeat_heredoc.c
@@ -12,7 +12,7 @@
 
 #include "parser.h"
 
-static char	*change_str(char *str, char *path, char *token)
+static char	*change_str(char *str, char *path, int token_len)
 {
 	int		i;
 	int		j;
@@ -26,7 +26,7 @@ static char	*change_str(char *str, char *path, char *token)
 	i += 2;
 	if (str[i] && str[i] == ' ')
 		i++;
-	while (str[i + j] && j < ft_strlen(token) - 1)
+	while (str[i + j] && j < token_len)
 		j++;
 	newstr = ft_strjoindel(newstr, "<");
 	newstr = ft_strjoindel(newstr, path);
@@ -37,12 +37,12 @@ static char	*change_str(char *str, char *path, char *token)
 }
 
 static int	return_heredoc(t_init *init, char *to_del,
-		char *heredoc, char *token)
+		char *heredoc, int token_len)
 {
 	char	*value;
 
 	value = create_newheredoc(heredoc);
-	init->str = change_str(init->str, value, token);
+	init->str = change_str(init->str, value, token_len);
 	ft_strdel(&to_del);
 	del_lex(init->lex);
 	return (1);
@@ -57,21 +57,25 @@ static int	no_heredoc(t_init *init, char *to_del, char *heredoc)
 	return (-1);
 }
 
-static void	delete_escape_heredoc(t_lex *lex)
+static int	delete_escape_heredoc(t_lex *lex)
 {
+	int token_len;
+
+	token_len = ft_strlen(lex->value);
 	lex->value = delete_esc(lex->value, ft_strlen(lex->value));
 	lex->value = ft_strjoindel(lex->value, "\n");
+	return (token_len);
 }
 
 int			repeat_heredoc(t_init *init, t_lex *tmp)
 {
 	int			ret;
+	int			token_len;
 	char		*line_tmp;
 	char		*heredoc;
 	static int	checkout = 0;
 
-	if (tmp->token == IO_HERE)
-		delete_escape_heredoc(tmp);
+	token_len = (tmp->token == IO_HERE) ? delete_escape_heredoc(tmp) : 0;
 	heredoc = (tmp->token == IO_HERE) ? ft_strdup("") : NULL;
 	while (tmp->token == IO_HERE)
 	{
@@ -81,7 +85,7 @@ int			repeat_heredoc(t_init *init, t_lex *tmp)
 		if (ret == 3)
 			return (no_heredoc(init, line_tmp, heredoc));
 		if (ft_strequ(line_tmp, tmp->value) || !line_tmp)
-			return (return_heredoc(init, line_tmp, heredoc, tmp->value));
+			return (return_heredoc(init, line_tmp, heredoc, token_len));
 		heredoc = (checkout) ? ft_strjoindel(heredoc, "\n") : heredoc;
 		heredoc = ft_strnjoindel(heredoc, line_tmp,
 				ft_strlen(heredoc) + ft_strlen(line_tmp) - 1);
